8c5dcea33f4e74e88d47e6a5db361af467e44d75
Jack <t.davis@student.isartdigital.com>
2024-07-11 09:35:28 +0200

    feat: added player movement /Navmesh package+ fix gitignore

--- RexyShortFilm/Assets/PlayerMovement.cs
+++ RexyShortFilm/Assets/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85ed5613e7d4bc3446b0dcc0dcb913b86baed2a3a0a40ae877f8c359d521b014
-size 1789
+oid sha256:759da408206d0081f643e00d3e8338f76c3bf7188fc6ff29695144e2f0397717
+size 2047

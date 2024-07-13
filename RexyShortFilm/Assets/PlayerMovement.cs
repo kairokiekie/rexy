7b0f2039bb2800ed22bd916a92c5c199abb18ea8
Jack <t.davis@student.isartdigital.com>
2024-07-14 00:18:21 +0200

    feat: added buggy animations into code :sunglasses:

--- RexyShortFilm/Assets/PlayerMovement.cs
+++ RexyShortFilm/Assets/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9fcba0b22f8acdb313374afd396865dba455f165535b7cb727e6d13af84037e
-size 3373
+oid sha256:e6df2d11217933f78630791b76c3f247493e40ee1748758f9f40ed2f447a967c
+size 3859

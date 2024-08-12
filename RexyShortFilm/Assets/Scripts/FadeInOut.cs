3b2f3186f63a95eb35c5c3aed261e1e6582514e1
Jack <t.davis@student.isartdigital.com>
2024-08-12 11:20:06 +0200

    feat: rob (ghost man spawn/despawn)

---
+++ RexyShortFilm/Assets/Scripts/FadeInOut.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:28bcf357a8cf819dc652155e7712baeb4477004f514766e103d796a8bcc25113
+size 1129
---
+++ RexyShortFilm/Assets/Scripts/KeyboardEvents.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0294e7015bbbd01bcbbf49be65e30fb045e31f4999296a72a76a17494d801921
+size 644

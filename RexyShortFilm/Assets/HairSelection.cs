eda65479f1e02c599f254e1fea2a6d3a7f042669
Jack <t.davis@student.isartdigital.com>
2024-08-30 13:46:03 +0200

    Feat: added scene changer buttons and menu screen (you can now quit w/out alt+ f4! )

--- RexyShortFilm/Assets/HairSelection.cs
+++ RexyShortFilm/Assets/HairSelection.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bef6775602ffefcafc1a4006b0e79457a80f96c4d0efaf36dad7b42b02683b6
-size 1360
+oid sha256:ed9ff1b1bb425c7106a7eed32227e7621727671199df51257b768ae6ee81b5c6
+size 1484
---
+++ RexyShortFilm/Assets/MenuManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a94e6151aed312f9fb9a8d1f43734fa559d414408244024534cf72b86e9e4d0a
+size 898

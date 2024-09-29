6b5829acf4b74bba819698196826d705da2dc827
Jack Davis <t.davis@student.isartdigital.com>
2024-09-29 17:25:06 +0200

    Script updates

--- RexyShortFilm/Assets/Scripts/PlayerMovement.cs
+++ RexyShortFilm/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2a848905469422d49ab3555d83588decd50a68a29bbd500cad9bcf8e02716fc
-size 5399
+oid sha256:f6d3cda93042e108c1f8a305c2f3aa403c7270c9106be612d8d0ebb7072398c5
+size 5771
---
+++ RexyShortFilm/Assets/Scripts/TimeManagement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1022411df0aaa80cb0e35c35de7de33a91f8961aa5d14c92731eb67648228c1b
+size 1873

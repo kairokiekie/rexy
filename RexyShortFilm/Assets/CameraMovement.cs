30ee60af80b556f2fccf1c17a1506bf2c69e876d
Jack Davis <t.davis@student.isartdigital.com>
2024-10-01 16:33:28 +0200

    Camera updates (pan, zoom)

--- RexyShortFilm/Assets/CameraMovement.cs
+++ RexyShortFilm/Assets/CameraMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a9f1e5518cc9a0e9256b90a27c9a0594db20b6ddbb27b0111eea70a2cc847a6
-size 629
+oid sha256:a3ac4ca49c76ef8ae2c4966cf5b26039b01c18c369f551f35978aa7025e31d0b
+size 1110

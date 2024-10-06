6405bf75f5d3904a4679ddf2c70f71e00cc0783b
Jack Davis <t.davis@student.isartdigital.com>
2024-10-06 10:34:20 +0200

    Assets: extra packs

--- RexyShortFilm/Assets/Scripts/needsManager.cs
+++ RexyShortFilm/Assets/Scripts/needsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46cde97ff8187e3e35995ba95f7aa767e48ab76b1ab5f26b21266d03282e2d56
-size 340
+oid sha256:acb5dcf4daba36903f23f34cab13610d12331a226219e1ec4f8ea7164e2f2fb0
+size 1165

51bd2875fa89b9cea733b113cb036b3cc7bd98e9
Jack <t.davis@student.isartdigital.com>
2024-10-22 08:28:46 +0200

    Tweaks (this one's for Mathis lmao) of textile mats, character DA, objects in scene, shortcuts

--- RexyShortFilm/Assets/NeedGlobalMangement.cs
+++ RexyShortFilm/Assets/NeedGlobalMangement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e13c1713642ab94094ec662c1cc98ae942ee2f1971af8197a1e12af5674c589
-size 483
+oid sha256:7d801a79163a7fb43bdd8857b562e064cdf01219ca1eac414cbb8417e73a619e
+size 563

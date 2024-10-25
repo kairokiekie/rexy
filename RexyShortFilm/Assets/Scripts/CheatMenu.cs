ee1c49d34690d1164c98dc39efd4cdbcbaeee0ac
Jack <t.davis@student.isartdigital.com>
2024-10-25 10:38:52 +0200

    Added pause feature, fixed floating pink

--- RexyShortFilm/Assets/Scripts/CheatMenu.cs
+++ RexyShortFilm/Assets/Scripts/CheatMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b80f061b534e599a64ec3cffbcd202a3baa46febc8634d033df40918dcf45bd3
-size 956
+oid sha256:d52c134d887b759d0cca76369cf04ee7a4baa0d4d14f80fd0db7e90145253680
+size 994
--- RexyShortFilm/Assets/Scripts/TimeManagement.cs
+++ RexyShortFilm/Assets/Scripts/TimeManagement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ac1806848a01b16aa3ba3bade226de9ba1bf681595d0ae06f86e3c56ee1fd89
-size 2897
+oid sha256:916464987258e055a1ef5a7cd481668c7b25c17984e8350d69bd4bf1e0fa09f1
+size 3593
